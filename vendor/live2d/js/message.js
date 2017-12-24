560ab35e6f78bd166a998c69b2e80187da3e8bd0
sani <khursani8@gmail.com>
2017-12-24 22:06:47 +0800

    fix height

--- vendor/live2d/js/message.js
+++ vendor/live2d/js/message.js
@@ -103,8 +103,11 @@ function showHitokoto(){
 function showMessage(text, timeout){
     if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
     console.log('showMessage', text);
-    $('.message').stop();
-    $('.message').html(text).fadeTo(200, 1);
+    message = $('.message')    
+    message.stop();
+    message.html(text).fadeTo(200, 1);
+    message.css('top',-message.height()+50);
+    
     if (timeout === null) timeout = 5000;
     hideMessage(timeout);
 }
