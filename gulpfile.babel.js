81d9977adbd6e427b8706684b46798077c6d9cf2
sani <khursani8@gmail.com>
2018-01-13 23:40:09 +0800

    gzip file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -57,7 +57,7 @@ gulp.task("css", () => (
 
 gulp.task("js", (cb) => {
   const myConfig = Object.assign({}, webpackConfig);
-  gulp.src(['vendor/**/*']).pipe(gulp.dest('dist/'))
+  gulp.src(['vendor/**/*']).pipe(gzip({ skipGrowingFiles : true })).pipe(gulp.dest('dist/'))
   webpack(myConfig, (err, stats) => {
     if (err) throw new gutil.PluginError("webpack", err);
     gutil.log("[webpack]", stats.toString({
